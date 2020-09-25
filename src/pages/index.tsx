13e314555befff1ddd25cded3673921bfb3e51f2
Andris Borbás <andris.borbas@gmail.com>
2020-09-25 04:41:27 +0200

    starter

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function IndexPage(): JSX.Element {
+  return (
+    <>
+      <div className="container mx-auto mt-8">
+        <h1 className="text-5xl text-center">Hello there</h1>
+      </div>
+    </>
+  );
+}
